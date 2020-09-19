1aa6b78e3fb5d8f7d92d5488efef6e2de30647ef
lmhgithi <lmhfox@foxmail.com>
2020-09-19 13:12:32 +0800

    resolve

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -1,44 +1,44 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+import java.util.Map;
+
 @ConfigurationProperties("mail")
+@Data
 public class MailConfig {
 
     private String hostname;
     private int port;
     private String from;
+    private List<String> defaultRecipients;
+    private Map<String, Boolean> additionalHeaders;
+    private Credentials credentials;
 
-    public String getHostname() {
-        return hostname;
-    }
-
-    public void setHostname(String hostname) {
-        this.hostname = hostname;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
 
     @Override
     public String toString() {
         return "MailConfig{" +
                 "hostname='" + hostname + '\'' +
-                ", port=" + port +
-                ", from='" + from + '\'' +
-                '}';
+                ",port=" + port +
+                ",from='" + from + '\'' +
+                ",defaultRecipients[0]='" + defaultRecipients.get(0) + '\'' +
+                ",defaultRecipients[1]='" + defaultRecipients.get(1) + '\'' +
+                ",additionalHeaders.redelivery=" + additionalHeaders.get("redelivery") +
+                ",additionalHeaders.secure=" + additionalHeaders.get("secure") + '\'' +
+                ",credentials.username='" + credentials.getUsername() + '\'' +
+                ",credentials.password='" + credentials.getPassword() + '\'' +
+                ",credentials.authMethod='" + credentials.getAuthMethod() + '\'' +
+                "}";
+
+    }
+
+    @Data
+    private static class Credentials {
+        private String username;
+        private String password;
+        private String authMethod;
     }
 }
